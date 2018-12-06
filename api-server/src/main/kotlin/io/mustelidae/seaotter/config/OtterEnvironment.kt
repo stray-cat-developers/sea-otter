bf5bb738c4c16e6766cefdb0087c8db82c490c26
fennec-fox <fennec.fox@titicaca-corp.com>
2018-12-06 11:13:37 +0900

    refactor: changed environment (path)

--- api-server/src/main/kotlin/io/mustelidae/seaotter/config/OtterEnvironment.kt
+++ api-server/src/main/kotlin/io/mustelidae/seaotter/config/OtterEnvironment.kt
@@ -11,18 +11,24 @@ class OtterEnvironment {
     var awsS3 = AwsS3()
 
     class LocalStorage {
-        var defaultPath: String = "/var/tmp"
+        var path = Path()
         var url: String = "http://localhost"
     }
 
     class AwsS3 {
         var bucket: String = "sea-otter"
-        var defaultPath: String = "multimedia/"
         var cloudFront = CloudFront()
+        var enabledSavingUnRetouchImage: Boolean = true
+        var path = Path()
 
         class CloudFront {
             var enabled: Boolean = false
             var url: String = "http://localhost"
         }
+    }
+
+    class Path {
+        var editedPath: String = "/image/edited"
+        var unRetouchedPath: String = "/image/unretouched"
     }
 }
