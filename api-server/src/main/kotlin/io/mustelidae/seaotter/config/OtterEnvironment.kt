8f3efdb2e548f975d23c314ef3dd7d1c725b45b1
fennec-fox <elijah17@gmail.com>
2019-10-17 18:15:57 +0900

    Update logging and app environment name

--- api-server/src/main/kotlin/io/mustelidae/seaotter/config/OtterEnvironment.kt
+++ api-server/src/main/kotlin/io/mustelidae/seaotter/config/OtterEnvironment.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConfigurationProperties(prefix = "sea-otter")
+@ConfigurationProperties(prefix = "app")
 class OtterEnvironment {
     var uploader: String = "local"
     var localStorage = LocalStorage()
