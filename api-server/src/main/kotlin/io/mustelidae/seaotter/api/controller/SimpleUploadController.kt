3295590469ec4ca6e03885ce506958353f132440
fennec-fox <elijah17@gmail.com>
2019-10-25 13:29:40 +0900

    Revise api name

--- api-server/src/main/kotlin/io/mustelidae/seaotter/api/controller/SimpleUploadController.kt
+++ api-server/src/main/kotlin/io/mustelidae/seaotter/api/controller/SimpleUploadController.kt
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
-@Api(tags = ["Image Uploader"])
+@Api(tags = ["Simple Image Upload"])
 @RestController
 @RequestMapping("/upload/simple")
 class SimpleUploadController
