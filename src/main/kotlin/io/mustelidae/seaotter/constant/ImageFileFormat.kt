f02a3fa10e37f8926070bf48b68f0daf50985de2
서오석 <fennec.fox@titicaca-corp.com>
2023-08-08 18:52:26 +0900

    Fix Upload by Blob Client  (#19)
    * Set the media type in the blob header.

--- src/main/kotlin/io/mustelidae/seaotter/constant/ImageFileFormat.kt
+++ src/main/kotlin/io/mustelidae/seaotter/constant/ImageFileFormat.kt
@@ -1,14 +1,46 @@
 package io.mustelidae.seaotter.constant
 
-enum class ImageFileFormat(val support: Boolean) {
-    JPG(true), JPEG(true), JFIF(false), // JPEG (Joint Photographic Experts Group) is a lossy compression method; JPEG-compressed images are usually stored in the JFIF (JPEG File Interchange Format) file format.
-    JP2(false), // JPEG 2000 is a compression standard enabling both lossless and lossy storage
-    TIFF(true), // The TIFF (Tagged Image File Format) format is a flexible format that normally saves eight bits or sixteen bits per color (red, green, blue) for 24-bit and 48-bit totals, respectively, usually using either the TIFF or TIF filename extension
-    GIF(false), // GIF (Graphics Interchange Format) is in normal use limited to an 8-bit palette, or 256 colors (while 24-bit color depth is technically possible).
-    BMP(true), // The BMP file format (Windows bitmap) handles graphic files within the Microsoft Windows OS
-    PNG(true), // The PNG (Portable Network Graphics) file format was created as a free, open-source alternative to GIF
-    PPM(false), PGM(false), PBM(false), PNM(false), // Netpbm format is a family including the portable pixmap file format (PPM), the portable graymap file format (PGM) and the portable bitmap file format (PBM)
-    WEBP(false), // WebP is a new open image format that uses both lossless and lossy compression.
-    HDR(true), // Most typical raster formats cannot store HDR data (32 bit floating point values per pixel component), which is why some relatively old or complex formats are still predominant here, and worth mentioning separately.
-    HEIF(false) // The High Efficiency Image File Format (HEIF) is an image container format that was standardized by MPEG on the basis of the ISO base media file format.
+
+enum class ImageFileFormat(
+    val support: Boolean,
+    /**
+     * Common MIME type
+     * @ref https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
+     */
+    val mediaType: String
+) {
+    // JPEG (Joint Photographic Experts Group) is a lossy compression method; JPEG-compressed images are usually stored in the JFIF (JPEG File Interchange Format) file format.
+    JPG(true, "image/jpeg"),
+    JPEG(true, "image/jpeg"),
+    JFIF(false, "image/jpeg"),
+
+    // JPEG 2000 is a compression standard enabling both lossless and lossy storage
+    JP2(false, "image/jpeg"),
+
+    // The TIFF (Tagged Image File Format) format is a flexible format that normally saves eight bits or sixteen bits per color (red, green, blue) for 24-bit and 48-bit totals, respectively, usually using either the TIFF or TIF filename extension
+    TIFF(true, "image/tiff"),
+
+    // GIF (Graphics Interchange Format) is in normal use limited to an 8-bit palette, or 256 colors (while 24-bit color depth is technically possible).
+    GIF(false, "image/gif"),
+
+    // The BMP file format (Windows bitmap) handles graphic files within the Microsoft Windows OS
+    BMP(true, "image/bmp"),
+
+    // The PNG (Portable Network Graphics) file format was created as a free, open-source alternative to GIF
+    PNG(true, "image/png"),
+
+    // Netpbm format is a family including the portable pixmap file format (PPM), the portable graymap file format (PGM) and the portable bitmap file format (PBM)
+    PPM(false, "image/ppm"),
+    PGM(false, "image/pgm"),
+    PBM(false, "image/pbm"),
+    PNM(false, "image/pnm"),
+
+    // WebP is a new open image format that uses both lossless and lossy compression.
+    WEBP(false, "image/webp"),
+
+    // Most typical raster formats cannot store HDR data (32 bit floating point values per pixel component), which is why some relatively old or complex formats are still predominant here, and worth mentioning separately.
+    HDR(true, "image/hdr"),
+
+    // The High Efficiency Image File Format (HEIF) is an image container format that was standardized by MPEG on the basis of the ISO base media file format.
+    HEIF(false,"image/heif")
 }
--- src/main/kotlin/io/mustelidae/seaotter/domain/uploader/AzureStorageUploader.kt
+++ src/main/kotlin/io/mustelidae/seaotter/domain/uploader/AzureStorageUploader.kt
@@ -2,6 +2,7 @@ package io.mustelidae.seaotter.domain.uploader
 
 import com.azure.core.util.BinaryData
 import com.azure.storage.blob.BlobServiceClientBuilder
+import com.azure.storage.blob.models.BlobHttpHeaders
 import com.azure.storage.common.StorageSharedKeyCredential
 import io.mustelidae.seaotter.config.AppEnvironment
 import io.mustelidae.seaotter.domain.delivery.Image
@@ -19,26 +20,26 @@ internal class AzureStorageUploader(
 ) : Uploader {
 
     override fun upload(image: Image): String {
-        val directoryPath = DirectoryPath(azureStorage.path, azureStorage.shardType, topicCode).apply {
-            append(image.isOriginal)
-        }
-
+        val directoryPath = DirectoryPath(azureStorage.path, azureStorage.shardType, topicCode)
         val credential = StorageSharedKeyCredential(azureStorage.accountName, azureStorage.accountKey)
 
         val blobClient = BlobServiceClientBuilder()
             .endpoint(azureStorage.endpoint)
             .credential(credential)
             .buildClient()
-            .getBlobContainerClient(directoryPath.getPath()).apply {
-                create()
-            }.getBlobClient(image.name)
+            .getBlobContainerClient(directoryPath.getPath())
+            .getBlobClient(image.name)
 
         val out = ByteArrayOutputStream()
         ImageIO.write(image.bufferedImage, image.getExtension(), out)
         val byteArray = out.toByteArray()
 
-        blobClient.upload(BinaryData.fromBytes(byteArray))
+        val headers = BlobHttpHeaders().apply {
+            contentType = image.imageFileFormat.mediaType
+        }
+        blobClient.setHttpHeaders(headers)
 
+        blobClient.upload(BinaryData.fromBytes(byteArray))
         return blobClient.blobUrl
     }
 }
